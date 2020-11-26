b38ae3d78fca4f21af8df36c80d77b5ec2a6c78c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-11-27 07:04:57 +0900

    grouping by location

--- FukuokaCOVID19/ContentView.swift
+++ FukuokaCOVID19/ContentView.swift
@@ -8,23 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    var patients: [Patient]
     var body: some View {
         VStack {
             Text("Hello, world! \(covidData.count)")
                 .padding()
-            List {
-                ForEach(covidData) { p in
-                    HStack {
-                        Text("\(p.id): \(p.release_date), \(p.location), \(p.age), \(p.gender)")
-                    }
-                }
-            }
+            LocationView(patients: groupByLocation(patients, threshold: 100))
+            // PatientsView(patients: patients)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(patients: covidData)
     }
+}
+
+struct PatientsView: View {
+    var patients: [Patient]
+    var body: some View {
+        List {
+            ForEach(patients) { p in
+                Text("\(p.id): \(p.release_date), \(p.location), \(p.age), \(p.gender)")
+            }
+        }
+    }
+}
+
+struct LocationView: View {
+    var patients: [(String, [Patient])]
+    var body: some View {
+        List {
+            ForEach(patients, id: \.self.0) { g in
+                HStack {
+                    Text("\(g.0): \(g.1.count)")
+                }
+            }
+        }
+    }
+}
+
+func groupByLocation(_ list: [Patient], threshold: Int) -> [(String, [Patient])] {
+    var dict: [String: [Patient]] = [:]
+    var unkonwn: [Patient] = []
+    for p in list {
+        if p.location == "" {
+            unkonwn.append(p)
+            continue
+        }
+        let loc: String = p.location
+        if nil == dict[loc] {
+            dict[loc] = []
+        }
+        dict[loc]!.append(p)
+    }
+    var list: [(String, [Patient])] = []
+    var misc: [Patient] = []
+    for i in dict {
+        if threshold <= i.value.count {
+            list.append((i.key, i.value))
+        } else {
+            misc.append(contentsOf: i.value)
+        }
+    }
+    list.sort(by: { $0.1.count > $1.1.count})
+    if 0 < misc.count {
+        list.append(("その他", misc))
+    }
+    if 0 < unkonwn.count {
+        list.append(("不明", unkonwn))
+    }
+    return list
 }
--- FukuokaCOVID19/Patient.swift
+++ FukuokaCOVID19/Patient.swift
@@ -8,17 +8,17 @@
 import Foundation
 import CodableCSV
 
-var covidData: [FukuokaCSV] = loadData()
+var covidData: [Patient] = loadData()
 
 let url = URL(string: "https://ckan.open-governmentdata.org/dataset/8a9688c2-7b9f-4347-ad6e-de3b339ef740/resource/c27769a2-8634-47aa-9714-7e21c4038dd4/download/400009_pref_fukuoka_covid19_patients.csv")!
 
-func loadData() -> [FukuokaCSV] {
+func loadData() -> [Patient] {
     if let data = try? Data(contentsOf: url) {
         do {
             let parsed = try CSVReader.decode(input: data)
-            var result: [FukuokaCSV] = []
+            var result: [Patient] = []
             for record in parsed {
-                let data: FukuokaCSV = FukuokaCSV(id: record[0],
+                let data: Patient = Patient(id: record[0],
                                                   code: record[1],
                                                   pref: record[2],
                                                   town: record[3],
@@ -28,15 +28,15 @@ func loadData() -> [FukuokaCSV] {
                                                   location: record[7],
                                                   age: record[8],
                                                   gender: record[9],
-                                                  patient_property: "",
-                                                  patient_status: "",
-                                                  patient_sympton: "",
-                                                  patient_abroad: "",
-                                                  appendix: "",
-                                                  recovered: "",
+                                                  patient_property: record[10],
+                                                  patient_status: record[11],
+                                                  patient_sympton: record[12],
+                                                  patient_abroad: record[13],
+                                                  appendix: record[14],
+                                                  recovered: record[15],
                                                   reason_unknown: record[16],
-                                                  deep_connected: "",
-                                                  been_abroad: ""
+                                                  deep_connected: record[17],
+                                                  been_abroad: record[18]
                 )
                 result.append(data)
             }
@@ -53,7 +53,7 @@ func loadData() -> [FukuokaCSV] {
 // 曜日, 発症_年月日, 居住地, 年代, 性別,
 // 患者_属性, 患者_状態, 患者_症状, 患者_渡航歴の有無フラグ, 備考,
 // 退院済フラグ, 感染経路不明, 濃厚接触者, 海外渡航歴有
-public struct FukuokaCSV: Codable, Identifiable {
+public struct Patient: Codable, Identifiable {
     public let id: String
     let code: String
     let pref: String
