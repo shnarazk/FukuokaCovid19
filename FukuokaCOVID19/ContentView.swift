da5e199c85f0edbfc9ecd41ede10620b52017ac0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-11-27 20:37:57 +0900

    add graphs
    	modified:   FukuokaCOVID19.xcodeproj/project.pbxproj
    	modified:   FukuokaCOVID19/Assets.xcassets/AppIcon.appiconset/Contents.json
    	new file:   FukuokaCOVID19/Assets.xcassets/AppIcon.appiconset/covid19-512.png
    	new file:   FukuokaCOVID19/Assets.xcassets/AppIcon.appiconset/covid19-513.png
    	modified:   FukuokaCOVID19/ContentView.swift
    	modified:   FukuokaCOVID19/Patient.swift

--- FukuokaCOVID19/ContentView.swift
+++ FukuokaCOVID19/ContentView.swift
@@ -10,9 +10,27 @@ import SwiftUICharts
 
 struct ContentView: View {
     var patients: [Patient]
+    @State var mode: Int = 1
     var body: some View {
         VStack {
-            LocationView(patients: groupByLocation(patients, threshold: 50))
+            Text("福岡県COVID19データ(\(patients.last(where: { _ in true })!.release_date.to_str())更新)")
+                .font(.title)
+            HStack {
+                // GraphView(data: groupBy(patients, mapper: { p in p.release_month }).map {(c, p) in (c, p.count)}, title: "月別")
+                GraphView(data: groupBy(patients, mapper: { p in p.release_date.to_str() }).map {(c, p) in (c, p.count)}, title: "詳細")
+                GraphView(data: groupByLocation(patients, threshold: 50).map {(c, p) in (c, p.count)}, title: "地域別")
+            }
+            .padding()
+            Picker(selection: $mode, label: EmptyView(), content: {
+                Text("月別詳細").tag(1)
+                Text("地域別詳細").tag(2)
+            })
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.horizontal, 10)
+            switch mode {
+            case 2: GroupView(patients: groupByLocation(patients, threshold: 50))
+            default: GroupView(patients: groupBy(patients, mapper: { p in p.release_month }))
+            }
             // PatientsView(patients: patients)
         }
     }
@@ -29,27 +47,27 @@ struct PatientsView: View {
     var body: some View {
         List {
             ForEach(patients) { p in
-                Text("\(p.id): \(p.release_date), \(p.location), \(p.age), \(p.gender)")
+                Text("\(p.id): \(p.release_date.to_str()) \(p.release_month) (\(p.elapsed)日前), \(p.location), \(p.age), \(p.gender)")
             }
         }
     }
 }
 
-struct LocationView: View {
+struct GroupView: View {
     var patients: [(String, [Patient])]
     var body: some View {
         VStack {
-            GraphView(data: patients.map {(c, p) in (c, p.count)}, title: "--")
-        NavigationView {
-            List {
-                ForEach(patients, id: \.self.0) { g in
-                    NavigationLink(destination: PatientsView(patients: g.1)) {
-                        Text("\(g.0): \(g.1.count)")
+            // GraphView(data: patients.map {(c, p) in (c, p.count)}, title: "--")
+            NavigationView {
+                List {
+                    ForEach(patients, id: \.self.0) { g in
+                        NavigationLink(destination: PatientsView(patients: g.1)) {
+                            Text("\(g.0): \(g.1.count)")
+                        }
                     }
                 }
+                .navigationTitle("都市別")
             }
-            .navigationTitle("都市別")
-        }
         }
     }
 }
@@ -58,8 +76,35 @@ struct GraphView: View {
     var data: [(String, Int)]
     var title: String
     var body: some View {
-        BarChartView(data: ChartData(values: data), title: title, form: ChartForm.extraLarge, dropShadow: false)
+        BarChartView(data: ChartData(values: data), title: title,
+                     form: CGSize(width: 800, height: 250), dropShadow: true, valueSpecifier: "%.0f人"
+        )
     }
+}
+
+func groupBy(_ list: [Patient], mapper: (Patient) -> String, order: ((String, [Patient]), (String, [Patient])) -> Bool = {$0.0 < $1.0 }) -> [(String, [Patient])] {
+    var dict: [String: [Patient]] = [:]
+    var unkonwn: [Patient] = []
+    for p in list {
+        let key: String = mapper(p)
+        if key == "" {
+            unkonwn.append(p)
+            continue
+        }
+        if nil == dict[key] {
+            dict[key] = []
+        }
+        dict[key]!.append(p)
+    }
+    var list: [(String, [Patient])] = []
+    for i in dict {
+        list.append((i.key, i.value))
+    }
+    list.sort(by: order)
+    if 0 < unkonwn.count {
+        list.append(("不明", unkonwn))
+    }
+    return list
 }
 
 func groupByLocation(_ list: [Patient], threshold: Int) -> [(String, [Patient])] {
@@ -93,4 +138,13 @@ func groupByLocation(_ list: [Patient], threshold: Int) -> [(String, [Patient])]
         list.append(("不明", unkonwn))
     }
     return list
+}
+
+extension Date {
+    func to_str() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ja_JP")
+        formatter.dateStyle = .medium
+        return formatter.string(from: self)
+    }
 }
--- FukuokaCOVID19/Patient.swift
+++ FukuokaCOVID19/Patient.swift
@@ -17,12 +17,12 @@ func loadData() -> [Patient] {
         do {
             let parsed = try CSVReader.decode(input: data)
             var result: [Patient] = []
-            for record in parsed {
+            for record in parsed.dropFirst() {
                 let data: Patient = Patient(id: record[0],
                                                   code: record[1],
                                                   pref: record[2],
                                                   town: record[3],
-                                                  release_date: record[4],
+                                                  release_date: parseDate(record[4]),
                                                   release_dayOfWeek: record[5],
                                                   sick_date: record[6],
                                                   location: record[7],
@@ -49,16 +49,44 @@ func loadData() -> [Patient] {
     return []
 }
 
+func parseDate(_ str: String) -> Date {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.dateFormat = "yyyy/MM/d"
+    if let date = formatter.date(from: str) {
+        return date
+    } else {
+        return formatter.date(from: "2020/01/01")!
+    }
+}
+
 // No, 全国地方公共団体コード, 都道府県名, 市区町村名, 公表_年月日,
 // 曜日, 発症_年月日, 居住地, 年代, 性別,
 // 患者_属性, 患者_状態, 患者_症状, 患者_渡航歴の有無フラグ, 備考,
 // 退院済フラグ, 感染経路不明, 濃厚接触者, 海外渡航歴有
 public struct Patient: Codable, Identifiable {
+    var elapsed: Int {
+        get {
+            let today = Date()
+            let cal = Calendar(identifier: .gregorian)
+            let days = cal.dateComponents([.day], from: release_date, to: today)
+            return days.day ?? 0
+        }
+    }
+    var release_month: String {
+        get {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "ja_JP")
+            formatter.dateFormat = "yyyy/MM"
+            return formatter.string(from: self.release_date)
+        }
+    }
+
     public let id: String
     let code: String
     let pref: String
     let town: String
-    let release_date: String
+    let release_date: Date
     
     let release_dayOfWeek: String
     let sick_date: String
